4bd85edfe4c543255dcd70fbf48e3185c5b24fa4
HOME_PC\masahiro <masa.target@outlook.jp>
2023-01-19 21:06:38 +0900

    久しぶりのｐｕｓｈ

---
+++ storage/framework/views/0a9c9910bd66dd4c0a68a8fe82efae58e7b9c073.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+<h2>機能を実装していきましょう。</h2>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.login', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\dawnSNS-Laravel-6.20.43\resources\views/posts/index.blade.php ENDPATH**/ ?>
